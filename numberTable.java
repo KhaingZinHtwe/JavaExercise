feb3788f309c1d0fb7d98a84ef0f939422d3b52f
Khaing Zin Htwe <dawkhaingzinhtwe@gmail.com>
2020-07-22 18:02:50 +0630

    Success

---
+++ numberTable.java
@@ -0,0 +1,16 @@
+package Date2272020;
+
+public class numberTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1; i<=10; i++) {
+			for(int j=1; j<=10; j++) {
+				System.out.print(i*j+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
